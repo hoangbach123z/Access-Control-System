6dd67c155dd998d605bf23c2a21f2a4d8161c3fb
Hoang Bach <bach.nh@smartmind.vn>
2024-12-10 13:31:00 +0700

    update code

--- src/main/java/com/bachnh/accesscontrolsystem/controller/LoginController.java
+++ src/main/java/com/bachnh/accesscontrolsystem/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.bachnh.accesscontrolsystem.controller;
 
+import fr.brouillard.oss.cssfx.CSSFX;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,13 +14,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+    private final Stage stage;
+    private double xOffset;
+    private double yOffset;
+    @FXML
+    HBox loginHeader;
     @FXML
     MFXTextField usernameTxt;
     @FXML
@@ -25,14 +36,34 @@ public class LoginController implements Initializable {
     MFXButton loginBtn;
     @FXML
     MFXButton cancelBtn;
+    @FXML
+    FontIcon closeIcon;
     public LoginController(Stage stage) {
-
+        this.stage = stage;
+        CSSFX.start();
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.CASPIAN)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         login();
     }
     private void login() {
+        closeIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> Platform.exit());
+        loginHeader.setOnMousePressed(event -> {
+            xOffset = stage.getX() - event.getScreenX();
+            yOffset = stage.getY() - event.getScreenY();
+        });
+
+        loginHeader.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() + xOffset);
+            stage.setY(event.getScreenY() + yOffset);
+        });
         loginBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             try {
                 // Đóng cửa sổ hiện tại
